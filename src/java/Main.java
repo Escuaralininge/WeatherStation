b311b6f0da4bbd333182002eca47786860235abf
Escuaralininge <36053817+Escuaralininge@users.noreply.github.com>
2019-04-30 20:33:54 +0200

    Update Main.java

--- src/java/Main.java
+++ src/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main (String [] args) {
 
 		System.out.println("WeatherStation!");
+		URLManager urlManager = new URLManager("https://www.archlinux.org/");
+		System.out.println(urlManager.read());
 
 	}
 
